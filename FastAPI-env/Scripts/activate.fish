10c89b994b195eda068a4aa983ac3ea1fac31072
Neha Nayak <160911567+NehaNayak73@users.noreply.github.com>
2024-10-12 08:28:14 +0530

    Initial project setup

--- FastAPI-env/Scripts/activate.fish
+++ FastAPI-env/Scripts/activate.fish
@@ -33,11 +33,11 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "C:\Users\bsakh\Documents\Git\fastAPI-Project\FasTAPI-env"
+set -gx VIRTUAL_ENV "C:\Users\bsakh\Desktop\FastAPI project\fastapi-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/Scripts" $PATH
-set -gx VIRTUAL_ENV_PROMPT "FasTAPI-env"
+set -gx VIRTUAL_ENV_PROMPT "fastapi-env"
 
 # Unset PYTHONHOME if set.
 if set -q PYTHONHOME
@@ -57,7 +57,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s(%s)%s " (set_color 4B8BBE) "FasTAPI-env" (set_color normal)
+        printf "%s(%s)%s " (set_color 4B8BBE) "fastapi-env" (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
